3cb551b40f5ebd4cd0a6f86a46dcfe789633769b
nytouu <capitaine7312@gmail.com>
2023-12-04 14:41:13 +0100

    click

--- Assets/Scripts/ClickManager.cs
+++ Assets/Scripts/ClickManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class ClickManager : MonoBehaviour
 {
+	[SerializeField] private MonsterManager monsters;
+	public bool autoClickEnabled;
+	public float delay = 1f;
+
+	private float clickPower = 1;
+	private float autoClickPower = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		StartCoroutine(AutoClick());
     }
 
     // Update is called once per frame
@@ -17,4 +24,14 @@ public class ClickManager : MonoBehaviour
 			Debug.Log("clicked");
 		}
     }
+
+	private IEnumerator AutoClick() {
+		while (autoClickEnabled) {
+			monsters.currentMonster.Attack(clickPower);
+			yield return new WaitForSeconds(delay);
+		} 
+		while (!autoClickEnabled) {
+			yield return new WaitForSeconds(delay * 5);
+		}
+	}
 }
---
+++ Assets/Scripts/MonsterManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterManager : MonoBehaviour
+{
+	public Monster currentMonster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMonster = new Monster();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
